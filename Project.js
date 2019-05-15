6f157619014405af2416b50a665bf563251111bc
krisibo01 <35684932+krisibo01@users.noreply.github.com>
2019-05-15 23:36:08 +0300

    final project
    this is the last version of my project

--- Project.js
+++ Project.js
@@ -1,3 +1,20 @@
+var images = [];
+images[0] = "1.jfif";
+images[1] = "2.jfif";
+images[2] = "3.jfif";
+images[3] = "4.png";
+images[4] = "5.jfif";
+var currentImg = 0;
+var img = document.getElementById("positionImg");
+var headers = [];
+headers[0] = "P2703B Plastic L96 Spring Sniper Rifle";
+headers[1] = "APS M40A3 Bolt Action Airsoft Sniper Rifle 550";
+headers[2] = "ARES DSR-1 Gas Sniper Rifles";
+headers[3] = "L96 AWP FH Sniper Rifle";
+headers[4] = "SW-04 ARMY sniper rifle";
+
+var text = document.getElementById("positionText");
+
 function changeTextDisplay(elementClass) {
     var elements = document.getElementsByClassName(elementClass);
     for (let i = 0; i < elements.length; i++) {
@@ -8,6 +25,18 @@ function changeTextDisplay(elementClass) {
             elements[i].style.display = 'block';
         }
     }
-	
 
+
+}
+
+function nextImage() {
+    currentImg = currentImg >= images.length - 1 ? 0 : ++currentImg;
+    img.src = images[currentImg];
+    text.innerHTML = headers[currentImg];
+}
+
+function previousImage() {
+    currentImg = currentImg <= 0 ? images.length - 1 : --currentImg;
+    img.src = images[currentImg];
+    text.innerHTML = headers[currentImg];
 }
